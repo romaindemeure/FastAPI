b16b17ea7194f0da6e480b881a2f4a8323e38ec3
Demeure romain <demeureromain4@gmail.com>
2022-06-12 18:35:35 +0200

    add comment on database.py

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,12 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+# create database
 SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 
+# create_engine for use database with database url and connect arguments
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
+# Local class which is going to be an instance of a database session
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# This Base it's used for create each model of database
 Base = declarative_base()
